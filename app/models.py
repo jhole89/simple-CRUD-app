ab6542caba6783442bb1cd796f5050e336d04954
jhole89 <joel.lutman@gmail.com>
2019-06-02 22:26:43 +0100

    add email model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,24 @@
 from app import db, ma
 
 
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
+
+    def __repr__(self):
+        return '<Email {}>'.format(self.email)
+
+
+class EmailSchema(ma.ModelSchema):
+    class Meta:
+        model = Email
+
+
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.relationship('Email', backref="contact", cascade="all, delete-orphan", lazy='dynamic')
     first_name = db.Column(db.String(128))
     surname = db.Column(db.String(128))
 
